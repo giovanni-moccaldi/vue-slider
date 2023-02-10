7f58adc6572b3a5751f0cac76297e8245ed4a2c7
giovanni-moccaldi <giovannimoccaldi@gmail.com>
2023-02-10 15:51:54 +0100

    start

---
+++ object-carousel-main/js/main.js
@@ -0,0 +1,13 @@
+console.log("ok", Vue);
+const app = Vue.createApp({
+    data() {
+        return {
+            message: 'Hello Vue!hope you will make my life easier'
+
+        }
+
+    }
+});
+
+
+app.mount("#root");
